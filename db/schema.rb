edc02f10099f92a147422ff94d67a3dfa8d5238e
Greg Jopa <grjopa@gmail.com>
2012-12-20 11:16:12 -0800

    Add basic 'Events' RefineryCMS Extension

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121220190004) do
+ActiveRecord::Schema.define(:version => 20121220191254) do
+
+  create_table "refinery_events", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "position"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "refinery_images", :force => true do |t|
     t.string   "image_mime_type"
